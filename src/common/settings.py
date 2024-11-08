04dfc9e9a595f9a2c85d34b150856b8b01e13e32
Jimin Yoo <jmy0819_@naver.com>
2024-11-08 23:53:58 +0900

    feat: add database configs

--- src/common/settings.py
+++ src/common/settings.py
@@ -1,12 +1,15 @@
 import os
 from pathlib import Path
 
+from pydantic import computed_field, MySQLDsn
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
 
 
 class Config:
     env_file = Path(__file__).parent.parent.parent / "envs" / f".env.{os.getenv('ENV', 'local')}"
     env_file_encoding = "utf-8"
+    extra = "ignore"
 
 
 class AppSettings(BaseSettings):
@@ -18,4 +21,28 @@ class AppSettings(BaseSettings):
         pass
 
 
+class DatabaseSettings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_DATABASE: str
+
+    @computed_field
+    @property
+    def DB_URL(self) -> MySQLDsn:
+        return MultiHostUrl.build(
+            scheme="mysql+aiomysql",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            path=self.DB_DATABASE,
+        )
+
+    class Config(Config):
+        pass
+
+
 app_settings = AppSettings()
+database_settings = DatabaseSettings()
